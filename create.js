0f04fcfc46c7d14ea504ce5b96d575e7d352f6d9
crushingpurifiergullible <crushingpurifiergullible@gmail.com>
2020-06-23 19:41:21 -0400

    fixed create.js problem, try again

--- create.js
+++ create.js
@@ -2,10 +2,8 @@ import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-export const main = handler(async (context, callback) => {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
